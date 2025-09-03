6ed40f09eb400825a86c290c48ac1c150980e902
Hua-Sheng Shao <hua-shengshao@Hua-Shengs-MacBook-Pro.local>
2025-09-03 16:43:10 +0200

    a few changes

--- vendor/polygamma/polygammafortran.cc
+++ vendor/polygamma/polygammafortran.cc
@@ -3,11 +3,13 @@
 
 using namespace std;
 
-extern "C" {
+extern "C"
+{
   // It is important that the function or subroutine name should be in lower cases
   // for Fortran
-  complex<double> polygamma_(const int & n, const complex<double> * z) {
+  complex<double> polygamma_(const int &n, const complex<double> *z)
+  {
     // call PolyGamma[n,z] function
-    return pGamma(n,*z);
+    return pGamma(n, *z);
   }
 }
